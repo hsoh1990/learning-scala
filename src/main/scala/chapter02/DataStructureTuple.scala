31ecb5082e548bccdc794b6bef9499cd8d306f27
hsoh <wellstone@hanbat.ac.kr>
2018-05-09 21:30:10 +0900

    Scala 자료구조
    Tuple 기본 문법 및 사용법

---
+++ src/main/scala/chapter02/DataStructureTuple.scala
@@ -0,0 +1,8 @@
+package chapter02
+
+class DataStructureTuple {
+  def sampleTuple2 : Tuple2[Int, String] = Tuple2[Int, String](99, "String")
+  def simpleTuple2: (Int, String) = (99, "String")
+  def simpleTuple3: (Char, Char, String) = ('h', 's', "oh")
+
+}
